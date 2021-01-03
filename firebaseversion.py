9ac581809ec16713e07a3e39abc1f7a5a90eac02
Potriashka <54754814+Potriashka@users.noreply.github.com>
2021-01-03 18:30:19 +0200

    Update firebaseversion.py

--- firebaseversion.py
+++ firebaseversion.py
@@ -1,30 +1,43 @@
+#DOING ALL NEEDED IMPORTS
 from flask import Flask, session
 from random import randrange
 from os import system, name 
 from firebase import firebase
 
+#DON'T KNOW HOW TO CALL IT BUT WE NEED THESE THINGS
 app = Flask(__name__)
-app.config['url'] = 'sqlite:///users.sqlite3'
 app.secret_key = 'secret_key'
 
+#WE NEED IT TO GENERATE NUMBER FOR USER
 UserNum = "User " + str(randrange(10)) + ": "
 
+#THANKS TO PYTHON CODE WON'T WORK IF THIS LINE DOESN'T EXIST
 result = "SEND MESSAGE\n"
 
+#MAYBE WE'LL NEED IT
+i = 0
+
+#HERE'S OUR LOOP SO WE'LL BE ABLE TO SEND MESSAGES OVER AND OVER AGAIN
 while True: 
+    #IT'S "cls" FOR WINDOWS, HERE WE'LL CLEAR CONSOLE TO EVERYTHING LOOKS OK
     system('clear') 
-    
+
+    #ALL MESSAGES WILL BE HERE UP THE INPUT
     print(result)
 
+    #WE'RE CREATING DB FOR OUR MESSAGES
     fb = firebase.FirebaseApplication("https://dbcfv-60641-default-rtdb.europe-west1.firebasedatabase.app/", None)
 
-
+    #NOW WE'RE ASKING FOR MESSAGE AND ADD USER NUMBER
     message = input("Type your message:   ")
     Smessage = UserNum + message
 
+    #IT IS OUR DATA WITH MESSAGES
     data = {
 	    'Message':Smessage
     }
 
-    fb.post('dbcfv-60641-default-rtdb/Message', data)
-    result = fb.get('dbcfv-60641-default-rtdb/Message', '')
+    #HERE WE'RE TRYING TO GET !ALL! MESSAGES
+    path = fb.post('dbcfv-60641-default-rtdb/Message', data)
+    Npath = str(path)[10:30]
+    result = fb.get(f'dbcfv-60641-default-rtdb/Message/', '')
